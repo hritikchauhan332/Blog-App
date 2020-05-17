9b3c95608c852338ab7e0a5e8502c4d91cb0cf67
Hritik Chauhan <hritikchauhan332@gmail.com>
2020-05-17 16:55:14 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,109 @@
+var express        = require("express");
+var app        	   = express();
+var bodyparser 	   = require("body-parser");
+var mongoose   	   = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+//mongoose.connect("mongodb://localhost/blogapp",{useNewUrlParser:true , useUnifiedTopology : true});
+mongoose.connect("mongodb+srv://pckiller10:pckiller10@cluster0-fi1ni.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser:true , useUnifiedTopology : true}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+ });
+app.set("view engine" , "ejs");
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+var blogSchema = new mongoose.Schema({
+	title : String,
+	image : String,
+	body : String,
+	created : {type : Date , default : Date.now}
+});
+
+var Blog = mongoose.model("Blog",blogSchema);
+
+// Blog.create({
+// 	title : "Test Blog",
+// 	image : "https://images.unsplash.com/photo-1489924034176-2e678c29d4c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60",
+// 	body : "Hello this is a HUSKY"
+// });
+app.get("/",function(req,res){
+	res.redirect("/blogs");
+});
+app.get("/blogs",function(req,res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {blogs : blogs});
+		}
+	});
+});
+
+app.get("/blogs/new",function(req,res){
+	res.render("new");
+});
+
+app.post("/blogs",function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.create(req.body.blog ,function(err,newBlog){
+		if(err){
+			console.log("new");
+		} else{
+			res.redirect("/blogs");
+		}
+	});
+});
+
+app.get("/blogs/:id",function(req,res){
+	Blog.findById(req.params.id, function(err,foundBlog){
+		if(err)
+			{
+				res.redirect("/blogs");
+			}
+		else{
+			res.render("show",{blog : foundBlog});
+		}
+	});
+});
+
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+})
+
+
+app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+      if(err){
+          res.redirect("/blogs");
+      }  else {
+          res.redirect("/blogs/" + req.params.id);
+      }
+   });
+});
+
+
+app.delete("/blogs/:id", function(req, res){
+   Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+   })
+});
+
+app.listen(process.env.PORT || 3000 , process.env.IP, function(){
+	console.log("BLOG APP IS RUNNING.....");
+});
